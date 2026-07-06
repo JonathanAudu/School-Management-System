2077e9d728b89ca50c97881a155d7fecd59abab7
JonathanAudu <jaudu2@gmail.com>
2026-07-06 08:38:07 +0100

    Fix Next.js image domain for Railway

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,14 +1,30 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     unoptimized: true,
+//     remotePatterns: [
+//       {
+//         protocol: 'http',
+//         hostname: 'localhost',
+//         port: '8000',
+//         pathname: '/storage/**',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/storage/**',
+        protocol: "https",
+        hostname: "school-management-system-production-1396.up.railway.app",
+        pathname: "/storage/**",
       },
     ],
   },
